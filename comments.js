7a11d66f1d03cbbf4d65fdd6cc3c35c2f6dba6a3
yepstein-p <141742252+yepstein-p@users.noreply.github.com>
2023-08-14 21:30:36 +0000

    ss

--- comments.js
+++ comments.js
@@ -1,6 +1,28 @@
 // create web server
-const server = http.createServer(app);
 
-// start listening to incoming requests
-server.listen(port);
-console.log(`Server is running on port ${port}`);
+var server = http.createServer(function(req, res) {
+  // create request router
+  var router = new Router();
+
+  // register routes
+  router.get('/', function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<html><body><h1>Hello World!</h1></body></html>');
+  });
+
+  router.get('/foo', function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<html><body><h1>Foo!</h1></body></html>');
+  });
+
+  router.get('/bar', function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<html><body><h1>Bar!</h1></body></html>');
+  });
+
+  // dispatch request
+  router.dispatch(req, res);
+});
+
+server.listen(3000);
+
