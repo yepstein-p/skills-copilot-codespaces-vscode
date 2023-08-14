e2cb7100608af2ef5499ba4c48742ce51332d308
yepstein-p <141742252+yepstein-p@users.noreply.github.com>
2023-08-14 21:23:34 +0000

    try again

--- comments.js
+++ comments.js
@@ -1,15 +1,6 @@
 // create web server
-var server = http.createServer(function(request, response) {
-    // send the http header
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    // send the response body
-    response.end('Hello World\n');
-});
+const server = http.createServer(app);
 
-// listen on port 8080
-server.listen(8080);
-
-// print URL for accessing server
-console.log('Server running')
-
-
+// start listening to incoming requests
+server.listen(port);
+console.log(`Server is running on port ${port}`);
